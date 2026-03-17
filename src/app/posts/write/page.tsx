754a4ed38df9fe50b3fe6c38a1c494d78da0d339
alison0525 <ak501636@gmail.com>
2026-03-17 15:16:45 +0900

    step24 - 상세페이지로 이동(replace)

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -1,5 +1,11 @@
 "use client";
+
+import { fetchApi } from "@/lib/client";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+    const router = useRouter();
+
     const handleSubmit = (e: any) => {
         e.preventDefault();
 
@@ -12,28 +18,26 @@ export default function Home() {
         if (titleInput.value.length === 0) {
             alert("제목을 입력해주세요.");
             titleInput.focus();
+            return;
         }
 
         if (contentText.value.length === 0) {
             alert("내용을 입력해주세요.");
             contentText.focus();
+            return;
         }
 
-        const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-
-        fetch(`${baseUrl}/api/v1/posts`, {
+        fetchApi(`/api/v1/posts`, {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
             body: JSON.stringify({
                 title: titleInput.value,
                 content: contentText.value,
             }),
-        }).then((res) => res.json())
+        })
             .then((data) => {
                 console.log(data);
                 alert(data.msg);
+                router.replace(`/posts/${data.data.postDto.id}`);
             })
     };
 
@@ -42,9 +46,20 @@ export default function Home() {
         <>
             <h1 className="text-center">새 글 작성</h1>
             <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
-                <input type="text" name="title" placeholder="제목" className="border-2 rounded p-2" />
-                <textarea name="content" rows={10} placeholder="내용" className="border-2 rounded p-2" />
-                <button type="submit" className="border-2 rounded p-2">저장</button>
+                <input
+                    className="border border-gray-300 rounded p-2"
+                    type="text"
+                    name="title"
+                    placeholder="제목"
+                />
+                <textarea
+                    className="border border-gray-300 rounded p-2"
+                    name="content"
+                    placeholder="내용"
+                />
+                <button className="bg-blue-500 text-white p-2 rounded" type="submit">
+                    저장
+                </button>
             </form>
         </>
     );
