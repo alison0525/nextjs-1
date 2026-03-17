35c1cc45577f6436ede4e9f1191fc64052a5340e
alison0525 <ak501636@gmail.com>
2026-03-17 15:35:40 +0900

    step25 - 글 삭제기능 추가

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,11 +2,13 @@
 
 import { fetchApi } from "@/lib/client";
 import { PostDto } from "@/type/post";
-import { useParams } from "next/navigation";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Home() {
     const { id } = useParams();
+    const router = useRouter();
 
     const [post, setPost] = useState<PostDto | null>(null);
 
@@ -14,18 +16,42 @@ export default function Home() {
         fetchApi(`/api/v1/posts/${id}`).then(setPost);
     }, []);
 
+    const deletePost = (id: number) => {
+        fetchApi(`/api/v1/posts/${id}`, {
+            method: "DELETE",
+        }).then((data) => {
+            alert(data.msg);
+            router.replace("/posts");
+        });
+    }
+
+    if (post === null) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <>
-            {post === null ? <div>Loading...</div> : (
-                <div className="flex flex-col gap-8 items-center">
-                    <h1>{id}번 글 상세 보기</h1>
-                    <div>
-                        <div>번호 : {post.id}</div>
-                        <div>제목 : {post.title}</div>
-                        <div>내용 : {post.content}</div>
-                    </div>
-                </div>
-            )}
+            <h1>글 상세 보기</h1>
+
+            <div>
+                <div>번호 : {post.id}</div>
+                <div>제목 : {post.title}</div>
+                <div>내용 : {post.content}</div>
+            </div>
+
+            <div className="flex gap-4">
+                <Link className="border-2 p-2 rounded" href={`/posts/${post.id}/edit`}>
+                    수정
+                </Link>
+                <button
+                    className="border-2 p-2 rounded"
+                    onClick={() => {
+                        deletePost(post.id);
+                    }}
+                >
+                    삭제
+                </button>
+            </div>
         </>
     );
 }
